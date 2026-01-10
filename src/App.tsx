af2740449b80cd45427f370774f1c82fb804946a
Ryan Nardelli <ryannardelli12@gmail.com>
2026-01-10 15:06:28 -0300

    style: Added colors to porject and variables with value and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
     <>
-      <h1 className='flex items-center h-screen justify-center'>Hello World</h1>
+     <Header />
     </>
   )
 }
