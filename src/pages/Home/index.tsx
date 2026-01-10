cf4c5a9f252fd3cacc1ca4310bb08869b3801ec2
Ryan Nardelli <ryannardelli12@gmail.com>
2026-01-10 16:36:07 -0300

    fix: Fixes made on desginer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,8 +6,8 @@ import MainTemplate from "../../templates/MainTemplate";
 export function Home() {
   return(
     <MainTemplate>
-      <ContainerContent className="flex flex-col justify-center items-center h-screen">
-        <Heading title="Detecte conteúdo gerado por Inteligência Artificial" label="Envie um arquivo PDF ou cole um texto para analisar a probabilidade de ter sido criado por IA." />
+      <ContainerContent className="flex flex-col justify-center items-center w-full">
+        <Heading title="Detecte conteúdo gerado por Inteligência Artificial" label="Envie ou insira um conteúdo para analisar a probabilidade de ter sido criado por IA." />
         <TabPick />
       </ContainerContent>
     </MainTemplate>
